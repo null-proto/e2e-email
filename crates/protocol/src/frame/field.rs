a3d36ec5cb522d0cbad5d65a50ac773afc2ba888
null-proto <209804848+null-proto@users.noreply.github.com>
2025-10-13 21:12:12 +0530

    fixes kv

--- crates/protocol/src/frame/field.rs
+++ crates/protocol/src/frame/field.rs
@@ -9,17 +9,17 @@
 pub static  M_FILE_COUNT : &str = "file-count";
 
 /// from address (sender)
-pub static  M_FILE_FROM : &str = "file-from";
+pub static  M_FROM : &str = "file-from";
 
 /// to address (receiver)
-pub static  M_FILE_TO :&str = "file-to";
+pub static  M_TO :&str = "file-to";
+
+/// mail short title
+pub static  M_TITLE : &str = "file-title";
 
 
 // --- mail metadata ends
 // --- file metadata starts
-
-/// mail short title
-pub static  M_FILE_TITLE : &str = "file-title";
 
 /// file extension : after `.`(dot)
 pub static  F_FILE_EXT : &str = "file-ext";
--- crates/protocol/src/kv.rs
+++ crates/protocol/src/kv.rs
@@ -1,7 +1,11 @@
-use crate::{bytes::{Bytes, RawBytes}, error::Error, serde::Serde};
+use crate::{
+  bytes::{Bytes, RawBytes},
+  error::Error,
+  serde::Serde,
+};
 use std::{collections::HashMap, sync::Arc};
 
-pub struct Kv<'a>(HashMap<Bytes<'a>, RawBytes>,Arc<[u8]>);
+pub struct Kv<'a>(HashMap<Bytes<'a>, RawBytes>, Arc<[u8]>);
 
 pub struct KvBuilder<'a>(Vec<(&'a str, &'a str)>);
 
@@ -15,7 +19,9 @@ impl TryFrom<&[u8]> for Kv<'_> {
     while c < value.len() {
       if let Some(v) = value.get(c) {
         a += *v as usize + 1;
-      } else { break; }
+      } else {
+        break;
+      }
 
       if let Some(v) = value.get(a) {
         let key = RawBytes::new(value.clone(), c + 1, a);
@@ -26,7 +32,7 @@ impl TryFrom<&[u8]> for Kv<'_> {
         return Err(Error::InvalidFrame);
       }
     }
-    Ok(Kv(map , value.clone()))
+    Ok(Kv(map, value.clone()))
   }
 }
 
@@ -38,17 +44,24 @@ impl TryFrom<Box<[u8]>> for Kv<'_> {
     let mut c = 0usize;
     let mut a = 0usize;
     while c < value.len() {
-      if let Some(v) = value.get(c) {
-        a += *v as usize + 1;
-      } else { break; }
+      match value.get(c) {
+        Some(n) => {
+          a += 1 + *n as usize;
+        }
+        None => break,
+      }
 
-      if let Some(v) = value.get(a) {
-        let key = RawBytes::new(value.clone(), c + 1, a);
-        c += *v as usize + 1;
-        let value = RawBytes::new(value.clone(), a + 1, c);
-        map.insert(key.into(), value);
-      } else {
-        return Err(Error::InvalidFrame);
+      match value.get(a) {
+        Some(n) => {
+          let key = RawBytes::new(value.clone(), c + 1, a);
+          c = 1 + a + *n as usize;
+          let value = RawBytes::new(value.clone(), a + 1, c);
+          map.insert(key.into(), value);
+          a = c;
+        }
+        None => {
+          return Err(Error::InvalidFrame);
+        }
       }
     }
     Ok(Kv(map, value.clone()))
@@ -63,17 +76,24 @@ impl TryFrom<Vec<u8>> for Kv<'_> {
     let mut c = 0usize;
     let mut a = 0usize;
     while c < value.len() {
-      if let Some(v) = value.get(c) {
-        a += *v as usize + 1;
-      } else { break; }
+      match value.get(c) {
+        Some(n) => {
+          a += 1 + *n as usize;
+        }
+        None => break,
+      }
 
-      if let Some(v) = value.get(a) {
-        let key = RawBytes::new(value.clone(), c + 1, a);
-        c += *v as usize + 1;
-        let value = RawBytes::new(value.clone(), a + 1, c);
-        map.insert(key.into(), value);
-      } else {
-        return Err(Error::InvalidFrame);
+      match value.get(a) {
+        Some(n) => {
+          let key = RawBytes::new(value.clone(), c + 1, a);
+          c = 1 + a + *n as usize;
+          let value = RawBytes::new(value.clone(), a + 1, c);
+          map.insert(key.into(), value);
+          a = c;
+        }
+        None => {
+          return Err(Error::InvalidFrame);
+        }
       }
     }
     Ok(Kv(map, value.clone()))
@@ -82,17 +102,35 @@ impl TryFrom<Vec<u8>> for Kv<'_> {
 
 impl<'a> Kv<'a> {
   pub fn get(&'a self, k: &'a str) -> Option<&'a str> {
-    self.0.get(&k.into()).map(|i| i.try_str().ok() )?
+    self.0.get(&k.into()).map(|i| i.try_str().ok())?
   }
 
   pub fn len(&self) -> usize {
     self.0.len()
   }
+
+  pub fn build() -> KvBuilder<'a> {
+    KvBuilder(Default::default())
+  }
 }
 
 impl<'a> KvBuilder<'a> {
-  pub fn insert(&mut self, key: &'a str, value: &'a str) {
+  pub fn insert(mut self, key: &'a str, value: &'a str) -> Self {
     self.0.push((key, value));
+    self
+  }
+
+  pub fn finish(self) -> Kv<'a> {
+    let mut vec: Vec<u8> = Vec::new();
+    for i in self.0 {
+      let k = i.0.as_bytes();
+      let v = i.1.as_bytes();
+      vec.push(k.len() as u8);
+      vec.extend_from_slice(k);
+      vec.push(v.len() as u8);
+      vec.extend_from_slice(v);
+    }
+    Kv::try_from(vec).unwrap()
   }
 }
 
@@ -105,12 +143,31 @@ impl Serde for Kv<'_> {
 impl Serde for KvBuilder<'_> {
   fn serialize(&self) -> Vec<u8> {
     let mut buf = Vec::default();
-    for (k,v) in &self.0 {
-      buf.push(k.as_bytes().len() as u8);
-      buf.extend_from_slice(k.as_bytes());
-      buf.push(v.as_bytes().len() as u8);
-      buf.extend_from_slice(v.as_bytes());
+    for (k, v) in &self.0 {
+      let k = k.as_bytes();
+      let v = v.as_bytes();
+      buf.push(k.len() as u8);
+      buf.extend_from_slice(k);
+      buf.push(v.len() as u8);
+      buf.extend_from_slice(v);
     }
     buf
+  }
+}
+
+#[cfg(test)]
+mod unitest {
+  use crate::frame::field::*;
+  use crate::kv::Kv;
+
+  #[test]
+  fn kv_builder() {
+    let kv = Kv::build()
+      .insert(M_FROM, "me@me.me")
+      .insert(M_TO, "you@you.you")
+      .finish();
+
+    assert_eq!(kv.get(M_FROM).unwrap(), "me@me.me");
+    assert_eq!(kv.get(M_TO).unwrap(), "you@you.you");
   }
 }
